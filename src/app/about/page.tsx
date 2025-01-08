b2cb0bbb4fb11d1bcf5cadf4beea441f560b2b0e
kiswaafaq <kiswaafaque14@mail.com>
2025-01-08 13:14:07 +0500

    kiswas commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,32 @@
+export default function About(){
+    return(
+        <div>
+        <div className="about-page">
+  <h1 className="about-heading">About Comforty</h1>
+  <p className="about-paragraph">
+    <b>Welcome to <u>Comforty</u>,</b> your one-stop destination for beautifully designed, high-quality furniture! 
+    We specialize in offering a wide variety of <b>chairs, tables, and sofas</b> that combine style, comfort, and functionality to 
+    create the perfect ambiance for your home or office.
+  </p>
+
+  <p className="about-paragraph">
+    At <b><u>Comforty</u></b>, we believe that furniture is more than just decor—it’s a reflection of your personality 
+    and a key to your comfort. Our pieces are thoughtfully crafted to suit diverse tastes and spaces, ensuring there’s something for everyone. 
+    Whether you’re looking for a <b>cozy sofa</b> to relax on after a long day, a <b>stylish table</b> for your dining area, or an 
+    <b>ergonomic chair</b> for your workspace, <b><u>Comforty</u></b> has you covered.
+  </p>
+
+  <p className="about-paragraph">
+    <b>Our mission</b> is to transform ordinary spaces into extraordinary ones by providing furniture that not only looks 
+    great but feels even better. Each product is carefully selected for its quality, durability, and design, so you can enjoy it for years to come.
+  </p>
+
+  <p className="about-paragraph center">
+    <b>Explore <u>Comforty</u> and discover how we can make your living space more inviting, functional, and uniquely yours!</b>
+  </p>
+</div>
+   
+        </div>
+
+    )
+}
---
+++ src/app/pages/page.tsx
@@ -0,0 +1,7 @@
+export default function Pages(){
+    return(
+        <div>
+            <h1>Hi pages page</h1>
+        </div>
+    )
+}
