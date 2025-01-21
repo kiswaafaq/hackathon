2921b73ec0623493f439f339cab503640b43e36a
kiswaafaq <kiswaafaque14@mail.com>
2025-01-21 17:40:19 +0500

    Day 6 task completed

--- cypress/e2e/productListing.cy.js
+++ cypress/e2e/productListing.cy.js
@@ -1,13 +1,13 @@
 describe('Product Listing Tests', () => {
-    it('should display products on the homepage', () => {
-      cy.visit('/');
-      cy.get('.product-listing').should('exist');
-      cy.get('.product-card').should('have.length.at.least', 1);
-    });
-    
-    it('should add a product to the cart', () => {
-      cy.get('.add-to-cart-btn').first().click();
-      cy.get('.cart-count').should('contain', '1');
-    });
+  it('should display products on the homepage', () => {
+    cy.visit('/');
+    cy.get('.product-listing', { timeout: 10000 }).should('be.visible');
+    cy.get('.product-card').should('have.length.at.least', 1);
   });
-  
+
+  it('should add a product to the cart', () => {
+    cy.visit('/'); // Ensure navigation before action
+    cy.get('.add-to-cart-btn', { timeout: 10000 }).first().should('be.visible').click();
+    cy.get('.cart-count', { timeout: 10000 }).should('contain', '1');
+  });
+});
