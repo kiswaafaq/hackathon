fa1e3601d7f16c7ab02ef015b515e35eb1bb466c
kiswaafaq <kiswaafaque14@mail.com>
2025-01-21 18:47:15 +0500

    Day 6 task completed

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,10 @@
-import AnalyticsDashboard from '../../components/Dashboard/AnalyticsDashboard';
+import AnalyticDashboard from '../../components/dashboard/AnalyticDashboard.tsx';
+import React from 'react';
 
 const DashboardPage = () => {
   const data = { sales: 12000, traffic: 5000 };
 
-  return <AnalyticsDashboard data={data} />;
+  return <AnalyticDashboard sales={data.sales} traffic={data.traffic} />;
 };
 
 export default DashboardPage;
